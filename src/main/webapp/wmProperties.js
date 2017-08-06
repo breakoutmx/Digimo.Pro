53396a41afd3d089c169e029c9409c98a36c0b6a
root <root@40ee80ac6db9>
2017-08-06 06:29:01 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "DigiMo.Pro",
+  "homePage" : "Main",
+  "name" : "DigiMo_Pro",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
