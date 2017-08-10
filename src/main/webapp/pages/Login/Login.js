ec555937d84e702e1a0ba3d719060c2c4764874b
Bryan Price-McConnahea <bryan@breakout.mx>
2017-08-10 22:14:54 +0000

    Pre Prefab update New files added - [Register] Files modified - [Common,Login,HerokuMysqlDMP1,securityService,profiles,project-security.xml,app.variables.json,pages-config.json]

--- src/main/webapp/pages/Login/Login.js
+++ src/main/webapp/pages/Login/Login.js
@@ -1,37 +1,17 @@
-Application.$controller("LoginPageController", ["$scope", function($scope) {
+Application.$controller("LoginPageController", ["$scope", function ($scope) {
     "use strict";
 
-    /* perform any action on widgets within this block */
-    $scope.onPageReady = function() {
+    /* perform any action on widgets/variables within this block */
+    $scope.onPageReady = function () {
         /*
+         * variables can be accessed through '$scope.Variables' property here
+         * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
+         * $scope.Variables.loggedInUser.getData()
+         *
          * widgets can be accessed through '$scope.Widgets' property here
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
-         
-        var progress = 0;
-        var updateLoginCheckProgress = setInterval(function() {
-            progress = $scope.Widgets.progress1.datavalue !== 'undefined' ? parseInt($scope.Widgets.progress1.datavalue) + 10 : 0;
-            $scope.Widgets.progress1.datavalue = progress;
-            if (progress === 100) {
-                clearInterval(updateLoginCheckProgress);
-            }
-            console.log('Update Login Check Progress: ' + $scope.Widgets.progress1.datavalue);
-        }, 150);*/
+         */
     };
 
 }]);
-
-
-Application.$controller("login1Controller", ["$scope",
-    function($scope) {
-        "use strict";
-        $scope.ctrlScope = $scope;
-    }
-]);
-
-Application.$controller("loginFormController", ["$scope",
-    function($scope) {
-        "use strict";
-        $scope.ctrlScope = $scope;
-    }
-]);
